a5dda7151b397da3a91daec2337e7d11c4a0c4cb
hoodielive <hoodielive@gmail.com>
2019-08-05 19:24:08 -0400

    Worked on a Zombie and Player

---
+++ Humanoid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Humanoid : MonoBehaviour
+{
+
+    private int health;
+    protected int attackDamage;
+    private float movementSpeed;
+
+    public virtual void TakeDamage(int damageToTake)
+    {
+        health -= damageToTake;
+    }
+    public virtual int Attack => 0;
+
+    public virtual void Move()
+    {
+
+        transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+
+    public void Die()
+    {
+
+    }
+}
---
+++ Zombie.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : Humanoid
+{
+
+    private int poisonDamage = 3;
+    public override void Move()
+    {
+        base.Move();
+        transform.Translate(Vector3.left * 3 * Time.deltaTime);
+    }
+
+    public int AcidPukeAttack()
+    {
+        return attackDamage + poisonDamage; 
+    }
+}
