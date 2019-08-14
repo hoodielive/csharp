9ea706064544234ff4584f6bdf7fab3b88447375
hoodielive <hoodielive@gmail.com>
2019-08-13 23:09:56 -0400

    reference and value types

--- csharp-prac/language-basics/default-named.cs
+++ csharp-prac/language-basics/default-named.cs
@@ -4,12 +4,29 @@ class Appology
 {
 	static void Main(string[] args)
 	{
-		new Appology().MySum(k: 1, i: 3); 	
+		new Appology().MySum(k: 1, i: 3); 
+		Appology message = new Appology();
+	
+		Console.WriteLine(message.GetPrint());
+		
+		Appology setter = new Appology(); 
+		setter.Set(44);
 	}
 	
 	void MySum(int i, int j = 0, int k = 0)
 	{
 		Console.WriteLine(i * i + 2 * j + 3 * k); 
+	}
+	
+	string GetPrint()
+	{
+		return "Hello";
+	}
+	
+	void Set(int i) 
+	{ 
+		i = 10; 
+		Console.WriteLine(i);
 	}
 	
 }
