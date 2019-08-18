5168ed882b49ccee1674b8702760684673f72f21
hoodielive <hoodielive@gmail.com>
2019-08-17 20:28:17 -0400

    strings and concepts

---
+++ csharp-prac/language-basics/strings/MyStrings.cs
@@ -0,0 +1,15 @@
+using System; 
+
+public class MyStrings 
+{
+  public void Main(string[] args)
+  {
+    Console.WriteLine(message);
+  }
+
+  void IAM()
+  {
+    string message = "My string"; 
+  }
+}
+
