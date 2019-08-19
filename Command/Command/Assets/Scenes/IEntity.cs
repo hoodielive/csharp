c66856d0258bdc348ff82fe8ce6ada24573bb44a
hoodielive <hoodielive@gmail.com>
2019-08-19 19:33:48 -0400

    deleting all this filfhy code and starting over because of my broken focus over the last couple of weeks

--- Command/Command/Assets/Scenes/IEntity.cs
+++
@@ -1,5 +0,0 @@
-﻿public interface IEntity : MonoBehaviour
-{
-    Transform transform { get; }
-    void MoveFromTo(Vector3 startPosition, Vector3 endPosition);    
-}
--- old/csharp-prac/language-basics/hello.cs
+++
@@ -1,9 +0,0 @@
-using System; 
-
-public class GoodByeWorld 
-{
-	public static void Main(string[] args)
-	{
-		Console.WriteLine("GoodBye!!!"); 
-	}
-}
