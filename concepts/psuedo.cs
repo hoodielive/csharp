1287563c582af1bcc5167891e9b2ac4a6e834cb9
hoodielive <hoodielive@gmail.com>
2019-08-19 19:33:48 -0400

    deleting all this filfhy code and starting over because of my broken focus over the last couple of weeks

--- concepts/psuedo.cs
+++
@@ -1,9 +0,0 @@
-first number = get input from user 
-if first number not numeric then print warning 
-
-second number = get input from user 
-if second number not numeric then print warning 
-
-operation = get input from user 
-
-lower case the operation for case insensitivity
