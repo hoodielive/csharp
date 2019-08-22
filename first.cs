1772ebdfac803aa4e5199e2b2c700f23a9d27b09
hoodielive <hoodielive@gmail.com>
2019-08-21 21:00:25 -0400

    Changes
    Update First

--- first.cs
+++ first.cs
@@ -6,5 +6,13 @@ class Test
 	{
 		int x = 12 * 30;
 		Console.WriteLine(x);
+		Conole.WriteLine(FeetToInches (30));
+		Conole.WriteLine(FeetToInches (100));
+	}
+
+	static int FeetToInches(int feet)
+	{
+		int inches = feet * 12;
+		return inches;
 	}
 }
