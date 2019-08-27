6661fa9490e2dccd9de18978b5a7b81fa8cf57b8
hoodielive <hoodielive@gmail.com>
2019-08-26 22:19:02 -0400

    Literals

--- Literals.cs
+++ Literals.cs
@@ -6,15 +6,31 @@ namespace FirstZ
 	{
 		public static void Main(string[] args)
 		{
-			uint ui = 5U;
-			int i = 5; 
+			uint ui = 5U; // unsigned integer
+			int i = 5; // 
 			sbyte sb = 127; 
 			decimal m = 30.5M; 
 			double d = 30.5D; 
 			float f = 30.5F; 
 			long l = 5L; 
-			ulong ul = 5UL; 
+			ulong ul = 5UL;
 			
+			string s = "hello, this is a string literal"; 
+			
+			string sa = @"This path is: 
+			~/Clones/csharp";
+			
+			char c = 'h';
+			
+			byte b = 127; 
+			bool bo = true; 
+			
+			Console.WriteLine(bo); 
+			Console.WriteLine(b); 
+			
+			Console.WriteLine(c); 
+			Console.WriteLine(sa);
+			Console.WriteLine(s); 
 			Console.WriteLine(ui);
 			Console.WriteLine(i);
 			Console.WriteLine(sb);
