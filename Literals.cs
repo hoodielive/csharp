77713d407cb41153bd50ba9352f79d4b44e163f8
hoodielive <hoodielive@gmail.com>
2019-08-26 21:49:20 -0400

    Literals

---
+++ Literals.cs
@@ -0,0 +1,28 @@
+using System; 
+
+namespace FirstZ
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			uint ui = 5U;
+			int i = 5; 
+			sbyte sb = 127; 
+			decimal m = 30.5M; 
+			double d = 30.5D; 
+			float f = 30.5F; 
+			long l = 5L; 
+			ulong ul = 5UL; 
+			
+			Console.WriteLine(ui);
+			Console.WriteLine(i);
+			Console.WriteLine(sb);
+			Console.WriteLine(m);
+			Console.WriteLine(d);
+			Console.WriteLine(f);
+			Console.WriteLine(l);
+			Console.WriteLine(ul);
+		}
+	}
+}
