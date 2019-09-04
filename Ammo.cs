cbb0a9a6a87c69bcb46180a3d7200ea7f9efa796
hoodielive <hoodielive@gmail.com>
2019-09-03 21:54:33 -0400

    Ammo

---
+++ Ammo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    // Lifetime for ammo
+    public float AmmoLife = 2.0f; 
+
+    // speed of ammo 
+    public float AmmoSpeed = 10.0f; 
+
+    // Use this for initialization 
+    IEnumerator Start() 
+    {
+        // Wait for ammo life time 
+        yield return new WaitForSeconds(AmmoLife);
+
+        // Destroy Ammo 
+        Destroy(gameObject);
+    }
+
+    // Destroy on collision 
+    void OnCollisionEnter(Collision collision) 
+    {
+        if(collision.gameObject.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        transform.Translate(-Vector3.forward * AmmoSpeed * Time.deltaTime, Space.Self);
+    }
+    
+}
