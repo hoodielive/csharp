9f3140db3b148da3468b33469b648475f27753bf
hoodielive <hoodielive@gmail.com>
2019-09-04 22:26:58 -0400

    Fundamentals

---
+++ csharp_fundamentals/Fundamentals2.cs
@@ -0,0 +1,33 @@
+using System; 
+
+namespace ProgrammingGuide 
+{
+	public class CustomCalss
+	{
+		// Property
+		public int Number { get; set; } 
+
+		// Method
+		public int Multiply (int num) 
+		{
+			return num * Number; 
+		}
+
+		// Instance Constructor
+		public CustomClass()
+		{
+			Number = 0; 
+		}
+	}
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			CustomClass custClass = new CustomClass(); 
+			custClass.Number = 27;
+			int result = custClass.Multiply(24);
+			Console.WriteLine($"The result is {result}."); 
+		}
+	}
+}
