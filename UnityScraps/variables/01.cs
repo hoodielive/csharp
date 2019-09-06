d2be5e703a13ee6e084adb19299acedf7e44df91
hoodielive <hoodielive@gmail.com>
2019-09-05 20:05:45 -0400

    variables

--- UnityScraps/variables/01.cs
+++ UnityScraps/variables/01.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	// syntax for variables
 	// public (everyone) private (only script) protected ()
+	// strings, bools, float, int 
+	// name, opt (value)  
+
+	public string name = "Larry"; 
+	public int myAge = 19;
+	public string myLocation "everywhere";
 	void Start() 
 	{
-		
+		Debug.Log("My name is: " + name); 	
+		Debug.Log("My age is: " + age); 	
+		Debug.Log("My location is: " + myLocation); 	
 	}
 
 	void Update() 
