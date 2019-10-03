b12da87bdffaedb368caff3d96c4bb36ba7ce605
hoodielive <hoodielive@gmail.com>
2019-10-03 17:49:48 -0400

    okay lets go

---
+++ UnityScraps/Ask/Ask/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+
+namespace Ask
+{
+    class Program
+    {
+        static void Main()
+        {
+            string thing = "movie";
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.Write("What is your favorite thing " + thing + "? ");
+            string favoriteThing = Console.ReadLine();
+            Console.WriteLine("My name is "  + name + " and my favorite " + thing + " is " + favoriteThing + ". ");
+        }
+    }
+}
