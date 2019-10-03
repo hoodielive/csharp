f2879a1c753367d3dacf0294f3b555ffb2f303c5
hoodielive <hoodielive@gmail.com>
2019-10-03 19:38:25 -0400

    compiliation

---
+++ UnityScraps/Game/Game/Game.cs
@@ -0,0 +1,14 @@
+namespace Game
+{
+    public class Game
+    {
+        public static void Main()
+        {
+            Map map = new Map();
+            map.Width = 8;
+            map.Height = 5;
+
+            int area = map.Width * map.Height; 
+        }
+    }
+}
--- UnityScraps/Objects/Objects/Tower.cs
+++ UnityScraps/Objects/Objects/Tower.cs
@@ -1,2 +1,4 @@
 ﻿using System; 
 
+namespace Objects
+
