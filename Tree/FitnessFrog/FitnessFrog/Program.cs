eeeb0fc49deab123c7ec84328afb44f1c72c7a94
hoodielive <hoodielive@gmail.com>
2019-11-09 11:15:03 -0500

    making the app smarter.

--- Tree/FitnessFrog/FitnessFrog/Program.cs
+++ Tree/FitnessFrog/FitnessFrog/Program.cs
@@ -7,14 +7,66 @@ namespace FitnessFrog
         public static void Main(string[] args)
         {
             // Prompt the user for minutes exercised
+
+            int runningTotal = 0;
             
-            Console.Write("Please enter the amount of minutes you exercised: ");
+            bool keepGoing = true;
             
-            // Add minutes exercised to total
-            
-            // Display total minutes exercised to the screen 
-            
-            // Repeat until the user quits
+           while(keepGoing)
+           {
+                Console.Write("Please enter the amount of minutes you exercised or type 'quit' to exit: ");
+
+                string entry = Console.ReadLine();
+
+                if (entry == "quit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    try
+                    {
+                        int minutes = int.Parse(entry);
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value!");
+                            continue;
+                        }
+
+                        if (minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing, am I right?");
+                        }
+                        else if (minutes <= 30)
+                        {
+                            Console.WriteLine("Way to go bro..."); 
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("You are a ninja in training.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Okay, now you are just showing off.");
+                        }
+
+                        // Add minutes exercised to total
+
+                        runningTotal += minutes;
+                        
+                        // Display total minutes exercised to the screen 
+                
+                        Console.WriteLine("You've entered: " + runningTotal + " minutes");
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("This is not valid input.");
+                        continue;
+                    }
+
+                }
+           } 
+           Console.WriteLine("Goodbye!");
         }
     }
 }
