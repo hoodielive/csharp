4fddebdb4cd0f9e1a54ae877d64ff833231123a2
hoodielive <hoodielive@gmail.com>
2019-11-09 10:12:24 -0500

    ok.. lets start again... shall we

--- Tree/FitnessFrog/FitnessFrog/Program.cs
+++ Tree/FitnessFrog/FitnessFrog/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace FitnessFrog
+﻿using System;
+
+namespace FitnessFrog
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            // Prompt the user for minutes exercised
             
+            Console.Write("Please enter the amount of minutes you exercised: ");
+            
+            // Add minutes exercised to total
+            
+            // Display total minutes exercised to the screen 
+            
+            // Repeat until the user quits
         }
     }
 }
