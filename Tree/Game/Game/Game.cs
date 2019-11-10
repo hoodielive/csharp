a11b60ef068b30c63a84564522f44dce8c36620a
hoodielive <hoodielive@gmail.com>
2019-11-10 15:34:59 -0500

    MapLocation is a subclass - used base(x,y) -- super

--- Tree/Game/Game/Game.cs
+++ Tree/Game/Game/Game.cs
@@ -8,17 +8,23 @@ namespace Game
         {
             Map map = new Map(8, 5);
             
-            Point point = new Point(4, 2);
+            MapLocation point = new MapLocation(4, 2, map);
             bool isOnMap = map.OnMap(point);
             Console.WriteLine(isOnMap);
             
-            point = new Point(8, 5);
-            isOnMap = map.OnMap(point);
-            Console.WriteLine(isOnMap);
-
             point.DistanceTo(5, 5);
             
             Console.WriteLine(point.DistanceTo(5,5));
+            Console.WriteLine(point is MapLocation);
+
+            try
+            {
+                MapLocation noneMapLocation = new MapLocation(20, 20, map);
+            }
+            catch(Exception)
+            {
+               Console.WriteLine("Sorry, That map location is not on the map."); 
+            }
         }
     }
 }
