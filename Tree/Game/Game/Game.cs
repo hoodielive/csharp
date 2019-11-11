39c545042875a9abe3fc409229e47989f2cf255f
hoodielive <hoodielive@gmail.com>
2019-11-10 21:53:30 -0500

    cleaning Up

--- Tree/Game/Game/Game.cs
+++ Tree/Game/Game/Game.cs
@@ -11,9 +11,7 @@ namespace Game
             MapLocation point = new MapLocation(4, 2, map);
             bool isOnMap = map.OnMap(point);
             Console.WriteLine(isOnMap);
-            
             point.DistanceTo(5, 5);
-            
             Console.WriteLine(point.DistanceTo(5,5));
             Console.WriteLine(point is MapLocation);
 
