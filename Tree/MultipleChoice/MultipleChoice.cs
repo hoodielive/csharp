f772b722f783455d0b7be1a8173cc41926b99234
hoodielive <hoodielive@gmail.com>
2019-11-15 14:47:37 -0500

    arghh..

--- Tree/MultipleChoice/MultipleChoice.cs
+++ Tree/MultipleChoice/MultipleChoice.cs
@@ -9,23 +9,14 @@ namespace MultipleChoice
 
 			Console.Write("What is your favorite Language? ");
 			var fav_lang = Console.ReadLine().ToLower();
-
-			Console.Write("What is your secondary language? ");
-			var sec_lang = Console.ReadLine().ToLower(); 
-
 			fav_lang = TryAgain(fav_lang); 
 
 		}
 			static string TryAgain(string fav_lang)
 			{
-				if (fav_lang == " ")
+				if (fav_lang == "")
 				{
 					Console.WriteLine("If you're going to play this game, you have to enter something!");
-				}
-				else 
-				{
-
-					Console.WriteLine("You have indicated that your favorite language is: {0} ", fav_lang);
 				}
 				return fav_lang;
 			}
