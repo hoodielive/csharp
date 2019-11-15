b8d43e1c14b1960814a48a6280cec814bafa5e19
hoodielive <hoodielive@gmail.com>
2019-11-15 13:56:46 -0500

    Arrays

---
+++ Tree/ArraysForDays/ArraysForDays.cs
@@ -0,0 +1,13 @@
+using System; 
+
+namespace ArraysForDays 
+{ 
+	class Program 
+	{
+		static void Main(string[] args)
+		{
+			var students = new int[] { 80, 77, 47, 100, 83, 99, 85, 72 }; 
+			Console.WriteLine(students);
+		}
+	}
+}
