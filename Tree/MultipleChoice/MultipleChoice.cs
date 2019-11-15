1c9cb6cc7329af6e51cb7a91bc28e2a1d3e95c5f
hoodielive <hoodielive@gmail.com>
2019-11-15 13:20:12 -0500

    now lets work on input validation

--- Tree/MultipleChoice/MultipleChoice.cs
+++ Tree/MultipleChoice/MultipleChoice.cs
@@ -7,19 +7,31 @@ namespace MultipleChoice
 		static void Main()
 		{
 
-			Console.Write("What is your favorite Language?");
-			var fav_lang = Console.ReadLine();
+			Console.Write("What is your favorite Language? ");
+			var fav_lang = Console.ReadLine().ToLower();
 
-			Console.Write("What is your secondary language");
-			var sec_lang = Console.ReadLine(); 
+			Console.Write("What is your secondary language? ");
+			var sec_lang = Console.ReadLine().ToLower(); 
 
 			Console.WriteLine("You have indicated that your favorite language is: {0} ", fav_lang);
-			Console.WriteLine("You have indicated that your secondary language is {0} ", sec_lang);
+			Console.WriteLine("You have indicated that your secondary language is: {0} ", sec_lang);
 
+			while (fav_lang == "")
+			{
+				Console.WriteLine("If you're going to play this game, you have to enter something!");
+			}
 
-			if (fav_lang == "C++")
+			if (fav_lang == "c++")
 			{
 				Console.WriteLine("You are a Geek!"); 
+			}
+			else if(fav_lang == "haskell")
+			{
+				Console.WriteLine("You are a Mathematician.");
+			}
+			else 
+			{
+				Console.WriteLine("Do you code?");
 			}
 		}
 	} 
