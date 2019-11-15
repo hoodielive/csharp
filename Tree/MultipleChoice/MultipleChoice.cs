c3fb63a2ee65a7d368a069b4d416acc2056ccb1a
hoodielive <hoodielive@gmail.com>
2019-11-15 14:37:59 -0500

    arghh..

--- Tree/MultipleChoice/MultipleChoice.cs
+++ Tree/MultipleChoice/MultipleChoice.cs
@@ -13,27 +13,22 @@ namespace MultipleChoice
 			Console.Write("What is your secondary language? ");
 			var sec_lang = Console.ReadLine().ToLower(); 
 
-			Console.WriteLine("You have indicated that your favorite language is: {0} ", fav_lang);
-			Console.WriteLine("You have indicated that your secondary language is: {0} ", sec_lang);
+			fav_lang = TryAgain(fav_lang); 
 
-			while (fav_lang == "")
-			{
-				Console.WriteLine("If you're going to play this game, you have to enter something!");
-			}
-
-			if (fav_lang == "c++")
-			{
-				Console.WriteLine("You are a Geek!"); 
-			}
-			else if(fav_lang == "haskell")
-			{
-				Console.WriteLine("You are a Mathematician.");
-			}
-			else 
-			{
-				Console.WriteLine("Do you code?");
-			}
 		}
+			static string TryAgain(string fav_lang)
+			{
+				if (fav_lang == " ")
+				{
+					Console.WriteLine("If you're going to play this game, you have to enter something!");
+				}
+				else 
+				{
+
+					Console.WriteLine("You have indicated that your favorite language is: {0} ", fav_lang);
+				}
+				return fav_lang;
+			}
 	} 
 }
 
