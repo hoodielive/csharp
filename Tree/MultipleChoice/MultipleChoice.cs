61f1a964336865555a0b5a83c72ba0ac7c2b61c8
hoodielive <hoodielive@gmail.com>
2019-11-15 13:01:52 -0500

    Multiple Choice - Clean

--- Tree/MultipleChoice/MultipleChoice.cs
+++ Tree/MultipleChoice/MultipleChoice.cs
@@ -9,10 +9,11 @@ namespace MultipleChoice
 
 			Console.Write("What is your favorite Language?");
 			var fav_lang = Console.ReadLine();
-			Console.WriteLine("You have indicated that your favorite language is: {0} ", fav_lang);
 
 			Console.Write("What is your secondary language");
 			var sec_lang = Console.ReadLine(); 
+
+			Console.WriteLine("You have indicated that your favorite language is: {0} ", fav_lang);
 			Console.WriteLine("You have indicated that your secondary language is {0} ", sec_lang);
 		}
 	} 
