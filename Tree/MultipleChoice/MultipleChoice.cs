134d2fe684dfcfe9783871475f4ec30f3f40a192
hoodielive <hoodielive@gmail.com>
2019-11-15 12:59:49 -0500

    Multiple Choice

---
+++ Tree/MultipleChoice/MultipleChoice.cs
@@ -0,0 +1,20 @@
+using System; 
+
+namespace MultipleChoice
+{
+	class Program 
+	{
+		static void Main()
+		{
+
+			Console.Write("What is your favorite Language?");
+			var fav_lang = Console.ReadLine();
+			Console.WriteLine("You have indicated that your favorite language is: {0} ", fav_lang);
+
+			Console.Write("What is your secondary language");
+			var sec_lang = Console.ReadLine(); 
+			Console.WriteLine("You have indicated that your secondary language is {0} ", sec_lang);
+		}
+	} 
+}
+
