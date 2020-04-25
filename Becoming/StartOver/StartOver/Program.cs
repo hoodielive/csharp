c9a6fa8bf793c39039b0430243d822055e728d01
hoodielive <hoodielive@gmail.com>
2020-04-25 14:01:16 -0400

    C# has me for a bit

---
+++ Becoming/StartOver/StartOver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO.Compression;
+using System.Collections.Generic;
+
+namespace StartOver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 x = 5;
+            String x = "Larry";
+        }
+    }
+}
