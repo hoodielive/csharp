4fb9ab9c86a4c18a15ff0c5acef31dfd598dede8
hoodielive <hoodielive@gmail.com>
2020-05-05 09:12:38 -0400

    deleting cruft - all the cruft that Visual Studio adds is insane.

--- Csharpness/newhello.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class NewHello
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, New Aeon");
-    }
-}
--- Tree/Game/Game/Tower.cs
+++
@@ -1,7 +0,0 @@
-namespace Game
-{
-    class Tower
-    {
-                 
-    }
-}
