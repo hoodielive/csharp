b0dc57f00c4925ba5b530c45a1c304983ec47fd9
hoodielive <hoodielive@gmail.com>
2020-05-22 22:40:28 -0400

    C# class

---
+++ Ride/IceCream/IceCream/IceCream/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace IceCream
+{
+    public class MyCar
+    {
+        private int topSpeed;
+        private string color;
+
+        public MyCar(int passedTopSpeed, string passedColor)
+        {
+            topSpeed = passedTopSpeed;
+            color = passedColor;
+        }
+
+        public int GetTopSpeed()
+        {
+            return topSpeed;
+        }
+
+        public void PaintCar(string newColor)
+        {
+            color = newColor;
+        }
+    }
+}
