87e4b972afb1a8873e43a531ad32d85f15dc4e6f
hoodielive <hoodielive@gmail.com>
2020-05-25 21:43:47 -0400

    I have been coding in Csharp for the last 2 weeks working on Awo in unity. Reg Sim.

---
+++ csharp_fundamentals/GenericsOnDeck/GenericsOnDeck/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericsOnDeck
+{
+    class Program
+    {
+        private int genericMemberVariable;
+
+        public myGenericClass(int value)
+        {
+            genericMemberVariable = value;
+        }
+
+        public int genericMethod(int genericParameter)
+        {
+            Console.WriteLine("Parameter type:  {0}, value: {1} ", typeof(int).ToString(), genericParameter);
+            Console.WriteLine("Parameter type:  {0}, value: {1} ", typeof(int).ToString(), genericMemberVariable);
+
+            return genericMemberVariable;
+        }
+    }
+    
+    public int genericProperty { get; set; }
+}
