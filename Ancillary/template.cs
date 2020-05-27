f24ad3134e68e8c8a66045e27ae13c08cb00ae09
hoodielive <hoodielive@gmail.com>
2020-05-27 18:33:58 -0400

    C# template

---
+++ Ancillary/template.cs
@@ -0,0 +1,18 @@
+Modifier class ClassName 
+{
+	// Member Data 
+	accessSpecifier type var1;
+	accessSpecifier type var2;
+	accessSpecifier type var3;
+
+	// Methods
+	accessSpecifier returntype method1 (parameter1, parameter2, parameter3)
+	{
+		// Method Body
+	}
+
+	accessSpecifier returntype method2 (parameter1, parameter2, parameter3)
+	{
+		// Method Body
+	}
+}
---
+++ csharp_fundamentals/NoObstacles/NoObstacles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoObstacles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
