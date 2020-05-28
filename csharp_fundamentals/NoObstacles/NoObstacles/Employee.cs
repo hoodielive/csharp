6eade037e915732dac5403ca96abf78373f57dcd
hoodielive <hoodielive@gmail.com>
2020-05-27 22:05:41 -0400

    idleness..

---
+++ csharp_fundamentals/NoObstacles/NoObstacles/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+
+    static void Main(string[] args)
+    {
+        Employee employee = new Employee();
+        employee.Id = 1;
+        employee.FirstName = "Nick";
+        employee.LastName = "Wise";
+        employee.Address = "Somewhere in America";
+    }
+    public void Display()
+    {
+        Console.WriteLine("Employee Id: "  + Id);
+        Console.WriteLine("Name: " + FirstName + " " + LastName);
+        Console.WriteLine("Address: " + Address);
+    }
+}
---
+++ csharp_fundamentals/NoObstacles/NoObstacles/Engineer.cs
@@ -0,0 +1,11 @@
+class Engineer : Employee
+{
+    public Engineer(string fname, string lname, string address)
+    {
+        this.FirstName = fname;
+        this.LastName = lname;
+        this.Address = address;
+
+    }
+
+}
